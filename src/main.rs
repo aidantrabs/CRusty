12e0993f974894d5d3d061b046f3e47290651034
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-02-24 02:12:24 -0500

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs;
+use std::process;
+
+mod lexer;
+
+/*
+    @Description: Main function
+    @Params: None
+    @Returns: None
+*/
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    let input = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("Error reading file {}: {}", filename, e);
+            process::exit(1);
+        }
+    };
+
+    let tokens = lexer::lex(&input);
+    println!("{:?}", tokens);
 }
