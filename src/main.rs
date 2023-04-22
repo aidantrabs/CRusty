ab989a65e6feb5e7c1b4eaa07318ee1a4b320fa7
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-04-22 00:10:27 -0400

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::process;
+use std::fs::*;
+use std::io::prelude::*;
+
 mod lexer;
+use lexer::Lexer;
+// mod parser;
 
 /*
     @Description: Main function
@@ -15,5 +20,15 @@ fn main() {
     }
 
     let filename = &args[1];
-    lexer::run_lexical_analysis(filename.to_string());
+    
+    let mut buffer1 = String::new();
+    let mut buffer2 = String::new();
+    let mut file = File::open(filename).expect("Unable to open file");
+
+    file.read_to_string(&mut buffer2).expect("Unable to read file");
+    buffer1.push_str(&buffer2);
+
+    let tokens = Lexer::get_next_token(&mut buffer1);
+
+    println!("{:#?}", tokens);
 }
